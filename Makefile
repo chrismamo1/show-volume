8d7d649d7970730c8a0d47a2decea7b51fd7e846
chrismamo1 <christophermcalpine@gmail.com>
2016-02-26 03:46:25 -0700

    added a few targets to the Makefile, and largely fixed the ridiculous memory leakage appearing in the old functorial interface

--- Makefile
+++ Makefile
@@ -3,7 +3,10 @@ INSTALL_PATH?=/usr/local/bin/show-volume.native
 all: show-volume.native
 
 show-volume.native: show-volume.ml DataSources.ml
-	corebuild -use-ocamlfind -pkgs lwt.unix,lwt,camomile,humane_re,textutils,yojson show-volume.native
+	corebuild -cflag -g -use-ocamlfind -pkgs lwt.unix,lwt,camomile,humane_re,textutils,yojson show-volume.native
+
+show-volume.p.native: show-volume.ml DataSources.ml
+	corebuild -cflag -g -use-ocamlfind -pkgs lwt.unix,lwt,camomile,humane_re,textutils,yojson show-volume.p.native
 
 clean:
 	corebuild -clean
--- show-volume.ml
+++ show-volume.ml
@@ -19,66 +19,69 @@ let color_to_string = function
   | `Green -> "#00ff00"
   | `Yellow -> "#ffff00"
 
-let rec main ({utf8; colored; i3bar; n; volume_provider} as cfg) old_state = begin
+let main ({utf8; colored; i3bar; n; volume_provider} as cfg) old_state = begin
   let (module Data) = volume_provider in
-  Data.read_volume
-    (`Sink (Data.sink_of_int n))
-    old_state
-  >>= fun ({status = stat; volume = [| vol_lft; vol_rgt |] }) ->
-  let icon = if vol_lft > 0 then speaker_icon else mute_icon in
-  let icon_color =
-    if vol_lft = 0 then `Red
-    else begin
-      if vol_lft <= 29 then `Yellow else `Green
-    end
-  in
-  let indicator =
-    match utf8, vol_lft with
-    | true, v when v = 0 -> ""
-    | true, _ ->
-      let open CamomileLibrary in let open UPervasives in
-      let ch = Scanf.sscanf (escaped_utf8 solid_rect) "\\u%x" (fun x -> x) in
-      let ch' = Scanf.sscanf (escaped_utf8 outline_rect) "\\u%x" (fun x -> x) in
-      UTF8.init 10 (fun i ->
-        uchar_of_int (if i < (vol_lft) / 10 then ch else ch'))
-    | false, _ ->
-        Printf.sprintf "% 3d%%  " (vol_lft)
-  in
-  let indicator_color = if vol_lft <= 20 then `Yellow else `Green in
-  let icon_printer =
-    if colored
-    then Console.Ansi.(output_string [icon_color] stdout)
-    else output_string stdout in
-  let indicator_printer =
-    if colored
-    then Console.Ansi.(output_string [indicator_color] stdout)
-    else output_string stdout in
-  begin match i3bar with
-  | false ->
-        Printf.(printf "Sink #%d " n);
-        if utf8
-        then begin
-          icon_printer icon;
-          indicator_printer indicator;
+  let rec aux (utf8, colored, i3bar, n) old_state =
+      Data.read_volume
+        (`Sink (Data.sink_of_int n))
+        old_state
+      >>= fun ({status = stat; volume = [| vol_lft; vol_rgt |] }) ->
+      let icon = if vol_lft > 0 then speaker_icon else mute_icon in
+      let icon_color =
+        if vol_lft = 0 then `Red
+        else begin
+          if vol_lft <= 29 then `Yellow else `Green
         end
-        else indicator_printer indicator;
-        output_string stdout "\r";
-  | true ->
-      let fulltext = Printf.sprintf "Sink #%d: %s%s" n icon indicator in
-      let output = Yojson.Basic.(
-          `List [
-            `Assoc [
-              "name", `String "volume";
-              "instance", `String (string_of_int n);
-              "full_text", `String fulltext;
-              "color", `String (color_to_string icon_color);
-            ]
-          ]
-        ) in
-      output_string stdout (Yojson.Basic.to_string output ^ "\n");
-  end;
-  flush_all ();
-  main cfg (Some {status = stat; volume = [|vol_lft; vol_rgt|]}); (* pretty sure this is a tailcall *)
+      in
+      let indicator =
+        match utf8, vol_lft with
+        | true, v when v = 0 -> ""
+        | true, _ ->
+          let open CamomileLibrary in let open UPervasives in
+          let ch = Scanf.sscanf (escaped_utf8 solid_rect) "\\u%x" (fun x -> x) in
+          let ch' = Scanf.sscanf (escaped_utf8 outline_rect) "\\u%x" (fun x -> x) in
+          UTF8.init 10 (fun i ->
+            uchar_of_int (if i < (vol_lft) / 10 then ch else ch'))
+        | false, _ ->
+            Printf.sprintf "% 3d%%  " (vol_lft)
+      in
+      let indicator_color = if vol_lft <= 20 then `Yellow else `Green in
+      let icon_printer =
+        if colored
+        then Console.Ansi.(output_string [icon_color] stdout)
+        else output_string stdout in
+      let indicator_printer =
+        if colored
+        then Console.Ansi.(output_string [indicator_color] stdout)
+        else output_string stdout in
+      begin match i3bar with
+      | false ->
+            Printf.(printf "Sink #%d " n);
+            if utf8
+            then begin
+              icon_printer icon;
+              indicator_printer indicator;
+            end
+            else indicator_printer indicator;
+            output_string stdout "\r";
+      | true ->
+          let fulltext = Printf.sprintf "Sink #%d: %s%s" n icon indicator in
+          let output = Yojson.Basic.(
+              `List [
+                `Assoc [
+                  "name", `String "volume";
+                  "instance", `String (string_of_int n);
+                  "full_text", `String fulltext;
+                  "color", `String (color_to_string icon_color);
+                ]
+              ]
+            ) in
+          output_string stdout (Yojson.Basic.to_string output ^ "\n");
+      end;
+      flush_all ();
+      aux (utf8, colored, i3bar, n) (Some {status = stat; volume = [|vol_lft; vol_rgt|]}); (* pretty sure this is a tailcall *)
+  in
+  aux (utf8, colored, i3bar, n) old_state
 end
 
 let () =
